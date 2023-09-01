044b5060650294cb81d6ecaa4229e230eed3fbae
Redmomn <109732988+Redmomn@users.noreply.github.com>
2023-09-01 20:11:40 +0800

    update

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,31 +1,73 @@
-import { defineConfig } from "vuepress/config";
+import { defineUserConfig } from 'vuepress'
+import type { DefaultThemeOptions } from 'vuepress'
 
-export default defineConfig({
-    locales: {
-        '/':{
-            lang:'zh-CN',
-            title: '测试docs',
-            description: '欢迎来到我的文档',
-            base:'/test-docs/',
-            head: [
-                        ['link', { rel: 'icon', href: '/images/logo.png' }]
-                    ],
-                    alias: {
-                        '@alias': '/test-docs',
-                    },
-        }
-    }
+export default defineUserConfig<DefaultThemeOptions>({
+    lang: 'zh-CN',
+    title: '测试docs',
+    description: '欢迎来到我的文档',
+    base: '/test-docs/',
+    head: [
+        ['link', { rel: 'icon', href: 'https://www.lovenie.icu/blog/wp-content/uploads/2023/09/宁宁.jpg' }]
+    ],
+    plugins: [
+        ['@vuepress/back-to-top'],
+        [
+            '@vuepress/pwa',
+            {
+                skipWaiting: true
+            }
+        ],
+        [
+            '@vuepress/plugin-pwa-popup',
+            {
+                locales: {
+                    '/': {
+                        message: '发现新内容可用',
+                        buttonText: '刷新',
+                    }
+                }
+            }
+        ],
+        [
+            '@vuepress/plugin-google-analytics',
+            {
+            }
+        ],
+        [
+            "@vuepress/plugin-docsearch",
+            {
+                
+            }
+        ]
+    ],
+    
+    // 主题和它的配置
+    theme: '@vuepress/theme-default',
+    themeConfig: {
+        repo: 'Mrs4s/go-cqhttp',
+        docsRepo: 'ishkong/go-cqhttp-docs',
+        docsBranch: 'main',
+        docsDir: 'docs',
+        editLinkText: "编辑此页",
+        lastUpdated: true,
+        lastUpdatedText: '上一次编辑',
+        contributors: true,
+        tip: "提示",
+        warning: "注意",
+        danger: "警告",
+        backToHome: "返回首页",
+        // navbar: [
+        //     { text: 'Guide', children: ['/guide/', '/guide/quick_start.md', '/guide/config.md', '/guide/eventfilter.md', '/guide/file.md', '/guide/achieve.md', '/guide/docker.md'] },
+        //     { text: 'API', children: ['/api/', '/api/guild.md'] },
+        //     { text: 'Event', children: ['/event/', '/event/guild.md'] },
+        //     { text: 'CQ Code', link: '/cqcode/' },
+        //     { text: 'Guild', link: '/guild/'},
+        //     { text: 'Reference', children: ['/reference/', '/reference/data_struct.md'] },
+        //     { text: 'Advanced', link: '/advanced/'},
+        //     { text: 'FAQ', link: '/faq/' }
+        // ],
+        sidebar: "auto",
+    },
+
 })
 
-// module.exports = {
-//     lang: 'zh-CN',
-//     title: '测试docs',
-//     description: '欢迎来到我的文档',
-//     base: '/test-docs/',
-//     head: [
-//         ['link', { rel: 'icon', href: '/images/logo.png' }]
-//     ],
-//     alias: {
-//         '@alias': '/test-docs',
-//     },
-// }
